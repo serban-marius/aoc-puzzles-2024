0ca8dfbdc2473944c591ebf41125b2b380d5a6cc
Marius Serban <mserban@ext.laligatech.com>
2023-12-08 18:24:38 +0100

    Repo got a 'Pimp My Ride' :)

---
+++ README.md
@@ -0,0 +1,41 @@
+# Advent of Code 2023 Solutions
+
+This repository contains my personal solutions to the [Advent of Code 2023](https://adventofcode.com/2023) puzzles. Advent of Code is an annual set of Christmas-themed programming challenges that follow an advent calendar format.
+
+## Structure
+
+Each day's puzzle solution is stored in a separate directory named `dayXX` where `XX` is the day of the month. For example, the solution for Day 1 is in the `day01` directory.
+
+```
+day01/
+day02/
+day03/
+...
+day25/
+```
+
+Inside each `dayXX` directory, you will find at least two files:
+
+- `input.txt`: The puzzle input provided by Advent of Code.
+- `part1.js`: The JS script that solves the puzzle.
+
+## Usage
+
+To run a solution script, navigate to the corresponding `dayXX` directory and run the JS script. For example:
+
+```bash
+cd day01
+node part1.py
+```
+
+Make sure you have Node installed on your system.
+
+## Contributing
+
+Feel free to explore the solutions and discuss alternative approaches. If you'd like to contribute or suggest improvements, please open an issue or a pull request.
+
+## Author
+
+Marius Serban
+
+Happy coding!
---
+++ day01/part1.js
@@ -0,0 +1,73 @@
+const fs = require('node:fs');
+fs.readFile('input.txt', 'utf8', (err, input) => {
+    if (err) throw Error;
+
+    let inputArray = input.split('\n');
+
+    inputArray = inputArray.map(nine);
+    inputArray = inputArray.map(eight);
+    inputArray = inputArray.map(seven);
+    inputArray = inputArray.map(six);
+    inputArray = inputArray.map(five);
+    inputArray = inputArray.map(four);
+    inputArray = inputArray.map(three);
+    inputArray = inputArray.map(two);
+    inputArray = inputArray.map(one);
+    inputArray = inputArray.map(removeChar);
+
+    let sum = calculateSum(inputArray);
+
+    console.log(sum);
+});
+
+function calculateSum(array) {
+    let sum = 0;
+
+    array.forEach(num => {
+        let value = num.split('');
+        const calibrationValue = "" + value[0] + value[value.length - 1];
+        sum += +calibrationValue
+    })
+
+    return sum;
+}
+
+function removeChar(string) {
+    return string.replace(/\D/g, '');
+}
+
+function one(string) {
+    return string.replace(/one/g, 'o1e');
+}
+
+function two(string) {
+    return string.replace(/two/g, 't2o');
+}
+
+function three(string) {
+    return string.replace(/three/g, 't3e');
+}
+
+function four(string) {
+    return string.replace(/four/g, 'f4r');
+}
+
+function five(string) {
+    return string.replace(/five/g, 'f5e');
+}
+
+function six(string) {
+    return string.replace(/six/g, 's6x');
+}
+
+function seven(string) {
+    return string.replace(/seven/g, 's7n');
+}
+
+function eight(string) {
+    return string.replace(/eight/g, 'e8t');
+}
+
+function nine(string) {
+    return string.replace(/nine/g, 'n9e');
+}
